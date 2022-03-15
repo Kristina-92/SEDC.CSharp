eff94e5624c12a650860e1c9c510d520fc65425d
Kristina <36110315+Kristina-92@users.noreply.github.com>
2022-03-15 21:03:48 +0100

    Add files via upload

---
+++ SEDC.Oop.Class03.Homework/SEDC.Oop.Class03.Homework.StudentGroup/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SEDC.Oop.Class03.Homework.StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Make a new console application called StudentGroup
+            //Make 2 arrays called studentsG1 and studentsG2 and fill them with 5 student names.
+            //Get a number from the console between 1 and 2 and print the students from that group in the console.
+            //Ex: studentsG1["Zdravko", "Petko", "Stanko", "Branko", "Trajko"]
+            //Test Data:
+            //Enter student group: (there are 1 and 2 )
+            //1
+            //Expected Output:
+            //The Students in G1 are:
+            //Zdravko
+            //Petko
+            //Stanko
+            //Branko
+            //Trajko
+
+            string[] studentsG1 = new[] {"Zdravko", "Petko", "Stanko", "Branko", "Trajko"};
+            string[] studentsG2 = new[] { "Miki", "Marko", "Filip", "Andrej", "Martin" };
+
+            Console.WriteLine("Choose a number 1 or 2");
+            string groupNumber = Console.ReadLine();
+            bool isInputNumber = int.TryParse(groupNumber, out int number); 
+
+            if(isInputNumber)
+            {
+                if(number == 1)
+                {
+                    foreach (string name in studentsG1) {
+                        Console.WriteLine(name);
+                    }
+                }
+                else if(number == 2)
+                {
+                    foreach(string name in studentsG2)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You can enter only 1 or 2");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter number input");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SEDC.Oop.Class03.Homework/SEDC.Oop.Class03.Homework.SumOfEven/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace SEDC.Oop.Class03.Homework.SumOfEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make a console application called SumOfEven. Inside it create an array of 6 integers. Get numbers from the input, find and print the sum of the even numbers from the array:
+
+            //Test Data:
+            //Enter integer no.1:
+            //4
+            //Enter integer no.1:
+            //3
+            //Enter integer no.1:
+            //7
+            //Enter integer no.1:
+            //3
+            //Enter integer no.1:
+            //2
+            //Enter integer no.1:
+            //8
+            //Expected Output:
+            //The result is: 14
+
+
+            string[] arrayOfNumbers = new string[6];
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                Console.WriteLine("Enter Number " + (i + 1));
+                arrayOfNumbers[i] = Console.ReadLine();
+            }
+
+            int sum = 0;
+
+            foreach (string element in arrayOfNumbers)
+            {
+                bool isInputValid = int.TryParse(element, out int numberParsed);
+
+                if (numberParsed % 2 == 0)
+                {
+                    sum += numberParsed;
+                }
+            }
+            Console.WriteLine("\nThe sum of even numbers in array is: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
